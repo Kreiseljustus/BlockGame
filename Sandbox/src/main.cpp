1db1eb5a52be1ab409131b378b47aa5f77f5030f
Crisel <justus.walter-helk@web.de>
2026-08-06 17:14:58 +0200

    Added quad primitive provider

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -8,6 +8,8 @@
 #include <Rendering/Renderer.h>
 #include <Rendering/Backends/OpenGLBackend.h>
 
+#include <Rendering/PrimitiveProvier.h>
+
 using namespace Engine::Rendering;
 using namespace Engine;
 
@@ -46,6 +48,7 @@ int main(int arc, char* argv[]) {
 	TextureHandle tHandle = renderer.CreateTexture(tParams);
 
 	MeshHandle test_handle = renderer.CreateMesh(test_data);
+	MeshHandle quad = renderer.CreateMesh(GetUnitQuad());
 
 	Input input;
 	window.setInputHandler(&input);
@@ -80,9 +83,10 @@ int main(int arc, char* argv[]) {
 
 		for (int i = 0; i < 100; i++) {
 			glm::vec3 pos = {i * 3,0,0.7};
-			renderer.Submit(test_handle, {test_Shader_handle.handle, tHandle.handle}, {pos, {270,0,0}, {1,1,1}});
+			//renderer.Submit(test_handle, {test_Shader_handle.handle, tHandle.handle}, {pos, {270,0,0}, {1,1,1}});
 		}
 		
+		renderer.Submit(quad, {test_Shader_handle.handle, tHandle.handle}, {{0,0,0.5}, {0,0,0}, {1,1,1}});
 
 		renderer.End();
 
